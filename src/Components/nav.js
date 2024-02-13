fd509799b05eba4ec01a091343a6a1aca6c0aa48
Kevin Hanard <kevin.hanard@gmail.com>
2024-02-13 15:35:37 +1100

    feat: fixed logo display

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -57,35 +57,37 @@ const NavMenu = ({ mobileMenuOpen, setMobileMenuOpen }) => {
                       leaveFrom='opacity-100 translate-y-0'
                       leaveTo='opacity-0 translate-y-1'
                     >
-                      <Popover.Panel className='absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5'>
+                      <Popover.Panel className='absolute -left-8 top-full z-10 mt-3 w-screen max-w-sm overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5'>
                         {sublinks.map((sublink) => {
                           const { id, url, text, iconpath, iconfill } = sublink;
+
                           return (
                             <div className='p-4'>
                               <div
                                 key={id}
-                                className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50'
+                                className='group relative flex items-center gap-x-4 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50'
                               >
-                                <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white'>
-                                  <svg
-                                    width='16'
-                                    height='16'
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    className='h-6 w-6 text-gray-600 group-hover:text-indigo-600'
-                                    aria-hidden='true'
-                                  >
-                                    <path d={iconpath} fill={iconfill} />
-                                  </svg>
-                                </div>
+                                {iconpath && (
+                                  <div className='flex h-11 w-10 flex-none items-center justify-center group-hover:bg-white'>
+                                    <svg
+                                      width='16'
+                                      height='16'
+                                      xmlns='http://www.w3.org/2000/svg'
+                                      className='h-6 w-6 text-gray-600 group-hover:text-indigo-600'
+                                      aria-hidden='true'
+                                    >
+                                      <path d={iconpath} fill={iconfill} />
+                                    </svg>
+                                  </div>
+                                )}
                                 <div className='flex-auto'>
                                   <a
                                     href={url}
                                     className='block font-medium text-gray-900'
                                   >
                                     {text}
-                                    <span className='absolute inset-0' />
                                   </a>
-                                  <p className='mt-1 text-gray-600'>{text}</p>
+                                  {/* <p className='mt-1 text-gray-600'>{text}</p> */}
                                 </div>
                               </div>
                             </div>
