95c63630082ca421c1587fbf8b2de5af5bb80053
Kevin Hanard <kevin.hanard@gmail.com>
2024-02-13 11:12:34 +1100

    build

--- src/Components/main.js
+++ src/Components/main.js
@@ -7,13 +7,13 @@ import meet from '../assets/client-meet.svg';
 const Main = ({ mobileMenuOpen }) => {
   return (
     <section className='w-screen max-h-full'>
-      <article className='container mx-auto flex flex-col-reverse items-center md:flex-row md:gap-x-8 md:mt-4 px-6 items-center space-y-0 border-black'>
+      <article className='container mx-auto flex flex-col-reverse items-center sm:mt-16 sm:content-center md:flex-row md:gap-x-8 md:mt-4 px-6 items-center space-y-0 border-black'>
         {/* left item / text */}
-        <div className='flex flex-col mt-8 mb-2 mx-auto  md:w-1/2 md:mt-16 md:content-center md:h-full'>
+        <div className='flex flex-col mt-8 mb-2 mx-auto  md:w-1/2 md:mt-12 md:content-center md:h-full'>
           <h1 className='max-w-md my-3 text-4xl font-bold text-center md:hidden'>
             Make remote work
           </h1>
-          <h1 className='hidden md:block max-w-md font-bold md:text-left md:text-6xl md:my-6'>
+          <h1 className='hidden md:block max-w-md tracking-wide font-bold md:text-left md:text-6xl md:my-6'>
             Make <br></br> remote work
           </h1>
           <p className='max-w-sm my-3 text-center text-darkGreyish md:text-left md:my-6 md:max-w-md'>
